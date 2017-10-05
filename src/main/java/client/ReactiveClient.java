d0489bd649700210e9f00b9ef35fda70bba4b359
Ali Myftiu <ali.myftiu@videoplaza.com>
2017-10-05 13:00:08 +0200

    added client

---
+++ src/main/java/client/ReactiveClient.java
@@ -0,0 +1,41 @@
+package client;
+
+import dto.Event;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class ReactiveClient {
+
+   private static final String SERVICE_URL = "http://localhost:8080";
+
+   @Bean
+   WebClient client(){
+      return WebClient.create(SERVICE_URL);
+   }
+
+
+   @Bean
+   CommandLineRunner runner() {
+      return args -> {
+         client().get().uri("/events/subscribe")
+            .accept(MediaType.TEXT_EVENT_STREAM)
+            .exchange()
+            .flatMapMany(cr -> cr.bodyToFlux(Event.class))
+            .subscribe(System.out::println);
+      };
+   }
+
+   public static void main(String... args) {
+      new SpringApplicationBuilder(ReactiveClient.class)
+         .properties(Collections.singletonMap("server.port", "8081"))
+         .run(args);
+   }
+
+}
